77b0c12955b57010fcf32b35fabc3e9ecbb6c025
pierpaolo <pierpaolo@pierpaolo-microhal>
2021-03-31 17:03:51 +0200

    - Fix esempi format

--- lab/src/main/java/di/uniba/map/b/lab/stringheNumeri/TestFormat.java
+++ lab/src/main/java/di/uniba/map/b/lab/stringheNumeri/TestFormat.java
@@ -35,7 +35,7 @@ public class TestFormat {
         System.out.format("%f%n", pi);       // -->  "3.141593"
         System.out.format("%.3f%n", pi);     // -->  "3.142"
         System.out.format("%10.3f%n", pi);   // -->  "     3.142"
-        System.out.format("%-10.3f%n", pi);  // -->  "3.142"
+        System.out.format("%-10.3f%n", -pi);  // -->  "3.142"
         System.out.format(Locale.US, "%-10.4f%n%n", pi); // -->  "3.1416"
         System.out.format(Locale.ITALIAN, "%-10.4f%n%n", pi); // -->  "3,1416"
     }
